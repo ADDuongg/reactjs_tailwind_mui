a20bff3718daa4c6a706f13565b2cf87eb70b629
duong <aduong88999@gmail.com>
2024-05-09 16:10:57 +0700

    fix

--- src/modal/modalHeader.jsx
+++ src/modal/modalHeader.jsx
@@ -1,12 +1,26 @@
 import React from 'react';
-
+import { useNavigate, useLocation } from 'react-router-dom';
 const ModalHeader = (props) => {
+    const Navigate = useNavigate();
+    const location = useLocation();
     const { toggleModal, menuItem } = props.data
+    function handleToggle(name) {
+        console.log(name);
+
+        if (location.pathname.includes('home2') && name === 'Home') {
+            Navigate("/");
+        } else if ((location.pathname.includes('home') || location.pathname.includes(`${name.toLocaleLowerCase()}`)) && name === 'Home') {
+            Navigate("/home2");
+        } else {
+            Navigate(`/${name.toLocaleLowerCase()}`)
+        }
+
+    }
     return (
         <div className="modalHeader">
             <div>
                 {menuItem?.map((item, index) => (
-                    <div key={index} className="item-modal">{item} </div>
+                    <div key={index} className="item-modal" onClick={() => handleToggle(item)}>{item} </div>
                 ))}
 
             </div>
